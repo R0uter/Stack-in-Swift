4e297514872c4b0e3c00d73ec971c84508b65390
R0uter <lomyr.jo@gmail.com>
2016-01-24 17:04:43 +0800

    init

---
+++ Stack in Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Stack in Swift
+//
+//  Created by R0uter on 16/1/24.
+//  Copyright © 2016年 R0uter. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
